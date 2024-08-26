cb322c28a3b2d5d67bd1b7d14ac32a7977e0d10a
chairuoxuan <940336799@qq.com>
2024-08-27 00:04:39 +0800

    feat: jsx

---
+++ packages/react/src/jsx.ts
@@ -0,0 +1,62 @@
+// ReactElement
+import { REACT_ELEMENT_TYPE } from 'shared/ReactSymbol';
+import {
+	Type,
+	Key,
+	Ref,
+	Props,
+	ReactElement,
+	ElementType
+} from 'shared/ReactTypes';
+
+const ReactElement = function (
+	type: Type,
+	key: Key,
+	ref: Ref,
+	props: Props
+): ReactElement {
+	const element = {
+		$$typeof: REACT_ELEMENT_TYPE,
+		type,
+		key,
+		ref,
+		props,
+		_marks: 'my'
+	};
+	return element;
+};
+
+export const jsx = (type: ElementType, config: any, ...maybeChildren: any) => {
+	let key: Key = null;
+	let ref: Ref = null;
+	const props: Props = {};
+	for (const prop in config) {
+		const val = config[prop];
+		if (prop === 'key') {
+			if (val !== undefined) {
+				key = '' + val;
+			}
+			continue;
+		}
+		if (prop === 'ref') {
+			if (val !== undefined) {
+				ref = val;
+			}
+			continue;
+		}
+		if ({}.hasOwnProperty.call(config, prop)) {
+			props[prop] = val;
+		}
+	}
+	const maybeChildrenLength = maybeChildren.length;
+	if (maybeChildrenLength) {
+		if (maybeChildrenLength === 1) {
+			props.children = maybeChildren[0];
+		} else {
+			props.children = maybeChildren;
+		}
+	}
+	return ReactElement(type, key, ref, props);
+};
+
+export const jsxDEV = jsx;
